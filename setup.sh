b2c5e836e3be01cdca624cf897a59c73f9f4e6e9
Joshua Chen <chenjd98@163.com>
2019-04-22 09:33:21 -0700

    add conditional

--- setup.sh
+++ setup.sh
@@ -7,20 +7,24 @@ TMP=~/.tmux/plugins
 # get zsh
 if [ -z "$(which zsh)" ]; then
 	sudo apt install zsh
+	chsh -s /bin/zsh 
 fi 
 
-chsh -s /bin/zsh 
 
 # get tmux
-if [ -z "$(which tmux)"]; then
+if [ -z "$(which tmux)" ]; then
 	sudo apt install tmux 
 fi 
 
 # get oh-my-zsh
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+if [ ! -d "$OMZ" ]; then
+	sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+fi
 
 # get tpm
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+if [ ! -d "$TMP/tpm" ]; then
+	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi 
 
 # deploy configs
 cp $ENV/keitoku.zsh-theme $OMZ/themes/keitoku.zsh-theme
@@ -33,5 +37,5 @@ cp $ENV/vimrc ~/.vimrc
 cp $ENV/tmux.conf ~/.tmux.conf 
 
 
-sudo chmod +x $ENV/update.sh 
+# sudo chmod +x $ENV/update.sh 
 
