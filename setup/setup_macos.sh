e187f2c8be60c2b3df85ccddcb879f2f2f1ee199
jc <chenjd98@163.com>
2019-10-25 01:43:22 -0500

    add macos script

--- setup/setup_macos.sh
+++ setup/setup_macos.sh
@@ -1,3 +1,39 @@
 #! /usr/bin/env bash 
 
 OS=macos
+
+
+get_zsh() {
+    if ! [[ -x $(command -v zsh) ]]; then
+        brew install zsh zsh-completions
+        if [[ -x "/bin/zsh" ]]; then
+            chsh -s "/bin/zsh"
+        else 
+            chsh -s $(which zsh) 
+        fi
+        MSG+=(">>> installed zsh <<<")
+    else
+        MSG+=("=== zsh already installed ===")
+    fi 
+}
+
+
+get_curl() {
+    if ! [[ -x $(command -v curl) ]]; then
+        brew install curl
+        MSG+=(">>> installed curl <<<")
+    else
+        MSG+=("=== curl already installed ===")
+    fi 
+}
+
+
+get_nodejs() {
+    if ! [[ -x $(command -v node) ]]; then
+        brew install node
+        MSG+=(">>> installed nodejs <<<")
+    else 
+        MSG+=("=== nodejs already installed ===")
+    fi 
+}
+
--- setup/setup_ubuntu.sh
+++ setup/setup_ubuntu.sh
@@ -52,21 +52,6 @@ get_nvim() {
 }
 
 
-get_zsh() {
-    if ! [[ -x $(command -v zsh) ]]; then
-        sudo apt install zsh
-        if [[ -x "/bin/zsh" ]]; then
-            chsh -s "/bin/zsh"
-        else 
-            chsh -s $(which zsh) 
-        fi
-        MSG+=(">>> installed zsh <<<")
-    else
-        MSG+=("=== zsh already installed ===")
-    fi 
-}
-
-
 get_nodejs() {
     if ! [[ -x $(command -v node) ]]; then
         sudo apt install -y nodejs
@@ -85,45 +70,5 @@ get_python3() {
     else
         MSG+=('=== python3 already installed ===')
     fi
-}
-
-
-deploy_configs() {
-    MSG+=(">>> deploying zsh configs")
-    cp ./keitoku.zsh-theme $OMZ/themes/keitoku.zsh-theme
-    cp ./zshrc ~/.zshrc
-
-    MSG+=(">>> deploying tmux configs")
-    tic ./utils/xterm-256color-italic.terminfo 
-    cp ./tmux.conf ~/.tmux.conf 
-
-    mkdir -p ~/.config/nvim/
-    MSG+=(">>> deploying vim configs")
-    cp ./vimrc ~/.vimrc
-    cp ./init.vim ~/.config/nvim/init.vim 
-
-    # vim colorscheme
-    mkdir -p ~/.vim/colors/ 
-    mkdir -p ~/.config/nvim/colors/
-    cp ./vim-keitoku.vim ~/.vim/colors/ 
-    cp ./vim-keitoku.vim ~/.config/nvim/colors/
-}
-
-
-# get oh-my-zsh
-get_OMZ() {
-    if [[ ! -d $OMZ ]]; then
-        sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -) --unattended" 
-        MSG+=(">>> installed oh-my-zsh <<<")
-    else
-        MSG+=("=== oh-my-zsh already installed ===")
-    fi
-}
-
-# print log
-put_msg() {
-    for msg in "${MSG[@]}"; do
-        echo $msg
-    done
 }
 
